1b73a5c3bd2c3ec6f2948ce85c64c615c85c5acb
Flavian Reignault <flavian.reignault@gmail.com>
2025-02-06 23:40:34 +0100

    feat: data recovery and model training

---
+++ scripts/download_data.py
@@ -0,0 +1,15 @@
+import os
+import requests
+
+def download_file(url: str, save_path: str):
+    response = requests.get(url)
+    response.raise_for_status()
+    with open(save_path, "wb") as f:
+        f.write(response.content)
+    print(f"File downloaded and saved in {save_path}")
+
+if __name__ == "__main__":
+    url = "https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv"
+    os.makedirs("data", exist_ok=True)
+    save_path = os.path.join("data", "red_wine_quality.csv")
+    download_file(url, save_path)
---
+++ scripts/train_model.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+import joblib
+
+df = pd.read_csv('data/red_wine_quality.csv', sep=';')
+
+X = df.drop('quality', axis=1)
+y = df['quality']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = RandomForestRegressor(n_estimators=100, random_state=42)
+model.fit(X_train, y_train)
+
+joblib.dump(model, 'model/red_wine_model.pkl')
+print("Model trained and saved in model/red_wine_model.pkl")
