6176708a0858bed0c21af2e6e24f772f2b672b2e
Flavian Reignault <flavian.reignault@gmail.com>
2025-02-11 00:40:19 +0100

    fix: allow ssh

--- terraform/main.tf
+++ terraform/main.tf
@@ -50,6 +50,19 @@ resource "google_compute_firewall" "allow-http" {
   target_tags   = ["wine-quality-prediction"]
 }
 
+resource "google_compute_firewall" "allow-ssh" {
+  name    = "allow-ssh"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["wine-quality-prediction"]
+}
+
 resource "google_artifact_registry_repository" "wine-quality-prediction-api" {
   location      = "europe-west1"
   repository_id = "wine-quality-prediction-api"
